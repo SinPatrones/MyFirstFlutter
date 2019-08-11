6c34720ddae09de6d85787b5d79aadc4cfd71545
ArmandoH <sinpatrones1@gmail.com>
2019-08-10 20:38:27 -0500

    Primera parte del código ejemplo de la web - Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(Center(
-    child: Text(
-      'Hello World',
-      textDirection: TextDirection.ltr,
-    )
-  ));
+void main() =>  runApp(MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome To Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World!!'),
+        ),
+      ),
+    );
+  }
 }
