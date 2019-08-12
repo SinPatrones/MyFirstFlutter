37595f18fa680aa88ff488f6460c2172a5f1ccab
ArmandoH <sinpatrones1@gmail.com>
2019-08-11 21:00:54 -0500

    Se inserto un icono de corazón indicando que es una palabra favorita, y asi se guarda en un SET, video cap-15 G Coding Academy

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() =>  runApp(MyApp());
+void main() => runApp(MyApp());
 
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: RandomWords(),
@@ -15,41 +13,62 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return RandomWordsState();
   }
 }
 
-class RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-
+  final _saved = Set<WordPair>(); // Un SET para guardar las palabras favoritas, SET guarda elementos distintos
+  final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions()
-    );
+        appBar: AppBar(
+          title: Text('Startup Name Generator'),
+        ),
+        body: _buildSuggestions());
   }
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return ListView.builder(
-        itemBuilder: (context, i) {
-          if (i >= _suggestions.length){
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[i]);
-        },
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd){
+          return Divider();
+        }
+        if (i >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        final index = i ~/2;
+        return _buildRow(_suggestions[index]);
+      },
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(pair.asPascalCase),
+      title: Text(
+          pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null
+      ),
+      onTap: (){
+        setState(() {
+          if (alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
-
 }
